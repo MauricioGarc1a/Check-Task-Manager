8c3630024f6e9c20251c17a9a48b121d03e1b5c0
khalilgharbaoui <kaygeee@gmail.com>
2016-02-23 16:44:54 +0100

    MODEL done with rspec TDD

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -13,6 +13,38 @@ RSpec.describe Task, type: :model do
     it {should validate_inclusion_of(:priority). in_array(['high', 'normal', 'low']) }
   end
 
+  describe "allowed values for duedate" do
+    it {should allow_value(/\d{4}.\d{2}.\d{2}/).for(:duedate) }
+  end
+
+# it "accepts only a valid date" do
+#
+# date = "2000-11-11"
+# task = Task.new duedate: date
+# task.should be_valid
+# task.duedate.should == Date.parse(date)
+#
+# end
+
+
+
+#   describe "allow value of status to be true or false!!" do
+#   # This will work, but will give you a warning
+#   it {should validate_inclusion_of(:status).in_array([true, false])}
+# end
+
+
+
+#   RSpec::Matchers.define :be_date_with_our_format do |expected|
+#   match do |date|
+#     date =~ /\d{4}.\d{2}.\d{2}/
+#   end
+# end
+#
+# it "has a placed_at date" do
+#   bid.placed_at.should be_datetime_with_our_format
+# end
+
 
 
 end
