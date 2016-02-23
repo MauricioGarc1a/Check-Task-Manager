a6df7a19a4c7a0c9ed89b75e85d350ccb3601aec
irisbune <irisbune@gmail.com>
2016-02-23 12:40:10 +0100

    rollback and re-added whitelist

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,7 +7,12 @@ RSpec.describe Task, type: :model do
     it { is_expected.to validate_presence_of(:duedate) }
     it { is_expected.to validate_presence_of(:priority) }
     it { is_expected.to validate_presence_of(:status) }
-
   end
+
+  describe "allowed values for priority" do
+    it {should validate_inclusion_of(:priority). in_array(['high', 'normal', 'low']) }
+  end
+
+
 
 end
