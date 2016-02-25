4099e6c6a50fafae6a77ebe18c294aaa472ab873
khalilgharbaoui <kaygeee@gmail.com>
2016-02-25 16:29:10 +0100

    seeds, and params... controller needs cheking?

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,23 +2,27 @@ class ProjectsController < ApplicationController
   def index
     render json: {
       meta: {
-        count: Project.count
+        count: Project.count,
+        page: 0
       },
-      projects: Project.all
+      projects: Project.order(:title)
     }
-    end
+  end
+
+  def show
+    project = Project.find(params[:id])
+    render json: { project: project }
+  end
 
   def create
-          project = Project.new(project_params)
-          # todo.project = @project
-          if project.save
-            render json: { project: project }
-          else
-            render json: {
-              message: 'Could not create Project',
-              errors: project.errors
-            }, status: :unprocessible_entity
-          end
+    if project = Project.create(project_params)
+      render json: { project: project }
+    else
+      render json: {
+        message: "Could not create project",
+        errors: project.errors,
+      }, status: :unprocessible_entity
+    end
   end
 
   def update
@@ -28,8 +32,8 @@ class ProjectsController < ApplicationController
       render json: { project: project }
     else
       render json: {
-        message: 'Could not update Project',
-        errors: project.errors
+        message: "Could not update project",
+        errors: project.errors,
       }, status: :unprocessible_entity
     end
   end
@@ -41,7 +45,7 @@ class ProjectsController < ApplicationController
       render json: { project: nil }
     else
       render json: {
-        message: 'Could not destroy Project, please try again'
+        message: "Could not destroy project, please try again",
       }, status: :unprocessible_entity
     end
   end
@@ -49,6 +53,6 @@ class ProjectsController < ApplicationController
   private
 
   def project_params
-    params.require(:project).permit(:project_description, :duedate, :status, :project_id)
+    params.require(:project).permit(:name, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'tasks#index'
 
-  resources :projects do
+  resources :projects, except: [:new, :edit] do
     resources :tasks, except: [:new, :edit]
   end
 
